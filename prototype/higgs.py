9bcc262cdb7bac2b72fe6289b13606c8f0e16132
Joakim Soderberg <joakim.soderberg@gmail.com>
2013-12-22 17:10:52 +0100

    Some nicer output.

--- prototype/higgs.py
+++ prototype/higgs.py
@@ -4,40 +4,46 @@ import random
 import sys
 import os
 import glob
+#from matplotlib import pyplot as plt
 
-def resize_and_gray_image(img):
+###################################################
+
+snout = cv2.imread("HIGGS/snout3.png", 0)
+if not os.path.exists("HIGGS/output/"):
+    os.makedirs("HIGGS/output/");
+
+for infile in glob.glob("HIGGS/higgs*.png"):
+    path, filename_wext = os.path.split(infile)
+    filename, fileext = os.path.splitext(filename_wext)
+    img = cv2.imread(infile)
+
+    # Resize the image so it's easier to display.
     simg = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
     simg_gray = cv2.cvtColor(simg, cv2.COLOR_BGR2GRAY)
-    return simg, simg_gray
-
-def get_contours(img_gray):
-    ret, threshimg = cv2.threshold(simg_gray, 35, 255, 0)# cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    
+    cv2.imwrite("HIGGS/output/%s_01original%s" % (filename, fileext), simg)
+    cv2.imwrite("HIGGS/output/%s_02gray%s" % (filename, fileext), simg_gray)
+    
+    # Threshold.
+    ret, threshimg = cv2.threshold(simg_gray, 35, 255, 0)
+    cv2.imshow('Binary', threshimg)
+    cv2.imwrite("HIGGS/output/%s_03binary%s" % (filename, fileext), threshimg)
 
     # Noise removal.
     kernel = np.ones((3,3),np.uint8)
     nonoise_img = cv2.erode(threshimg, kernel, iterations = 3)
+    cv2.imshow('No noise', nonoise_img)
+    cv2.imwrite("HIGGS/output/%s_04nonoise%s" % (filename, fileext), nonoise_img)
 
-    # findContours changes the image, so make a copy.
-    thresh_img = nonoise_img.copy()
-
-    contours, hierarchy = cv2.findContours(nonoise_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    return (thresh_img, contours, hierarchy)
-
-###################################################
-
-for infile in glob.glob('HIGGS/higgs*.png'):
-
-    img = cv2.imread(infile)
-    snout = cv2.imread('HIGGS/snout3.png', 0)
-
-    simg, simg_gray = resize_and_gray_image(img)
-    thresh_img, contours, hierarchy = get_contours(simg_gray)
-    cv2.imshow('Binary', thresh_img)
-
-    matchres = cv2.matchTemplate(thresh_img, snout, cv2.TM_CCOEFF_NORMED)
+    # Match the snout with the binary noise reduced image.
+    matchres = cv2.matchTemplate(nonoise_img, snout, cv2.TM_CCOEFF_NORMED)
     (min_x, max_x, minloc, maxloc) = cv2.minMaxLoc(matchres)
     (x, y) = maxloc
 
+    cv2.imshow("match", matchres)
+    cv2.imwrite("HIGGS/output/%s_05tempmatch%s" % (filename, fileext), matchres)
+
+    # From some testing 0.9 seems like a good threshold.
     if (max_x >= 0.9):
         print "Match! %s" % max_x
         color = (0, 255, 0)
@@ -49,12 +55,20 @@ for infile in glob.glob('HIGGS/higgs*.png'):
     snout_w = snout.shape[1]
     snout_h = snout.shape[0]
     cv2.rectangle(simg, (x, y), (x + snout_w, y + snout_h), color, 0)
-    cv2.imshow("match", matchres)
 
     # Draw the final image.
-    cv2.drawContours(simg, contours, -1, color, 3)
     cv2.imshow('Final', simg)
+    cv2.imwrite("HIGGS/output/%s_06final%s" % (filename, fileext), simg)
 
+    """
+    plt.subplot(231),plt.imshow(simg,'gray'),plt.title('ORIGINAL')
+    plt.subplot(232),plt.imshow(simg_gray,'gray'),plt.title('GRAY')
+    plt.subplot(233),plt.imshow(threshimg,'gray'),plt.title('BINARY')
+    plt.subplot(234),plt.imshow(nonoise_img,'gray'),plt.title('NO-NOISE')
+    plt.subplot(235),plt.imshow(matchres,'gray'),plt.title('MATCH RESULT')
+    plt.subplot(236),plt.imshow(simg,'gray'),plt.title('MATCH')
+    """
+    
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
