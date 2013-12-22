1c40bc3251b5f2914a252e5b81c690273932981c
Joakim Soderberg <joakim.soderberg@gmail.com>
2013-12-22 22:39:23 +0100

    Initial commit for C program.

---
+++ test.c
@@ -0,0 +1,24 @@
+
+#include <cv.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	IplImage* snout = NULL;
+	IplImage* img = NULL;
+
+	if (argc < 3)
+	{
+		fprintf(stderr, "Usage: %s <snout image> <input image>\n");
+		return -1;
+	}
+
+	snout = cvLoadImage(argv[1]);
+	img = cvLoadImage(argv[2]);
+
+	cvShowImage("hej", img);
+	cvWaitKey(0);
+	printf("hej\n");
+	return 0;
+}
