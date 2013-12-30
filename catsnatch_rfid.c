8f20c301545fe46c00e023f97c3f15ee14f754d2
Joakim Soderberg <joakim.soderberg@gmail.com>
2013-12-30 01:14:13 +0000

    More work on RFID reader code.

--- catsnatch_rfid.c
+++ catsnatch_rfid.c
@@ -13,11 +13,28 @@
 
 #define _POSIX_SOURCE 1 // POSIX compliant source.
 
-int catsnatch_rfid_init(catsnatch_rfid_t *rfid, const char *serial_path, catsnatch_rfid_read_f read_cb)
+const char *catsnatch_rfid_errors[] = 
 {
+	"Command not understood",				// ?0
+	"Tag not present",						// ?1
+	"Tag failure to Read/Write",			// ?2
+	"Access to Block 0 not allowed",		// ?3
+	"Page address invalid for this tag"		// ?4
+};
+
+const char *catsnatch_rfid_error_str(int errorcode)
+{
+	return catsnatch_rfid_errors[errorcode % (sizeof(catsnatch_rfid_errors) / sizeof(char *))];
+}
+
+int catsnatch_rfid_init(const char *name, catsnatch_rfid_t *rfid, const char *serial_path, catsnatch_rfid_read_f read_cb)
+{
+	strcpy(rfid->name, name);
 	rfid->serial_path = serial_path;
 	rfid->fd = -1;
 	rfid->cb = read_cb;
+	rfid->bytes_read = 0;
+	rfid->offset = 0;
 
 	return 0;
 }
@@ -34,51 +51,111 @@ void catsnatch_rfid_destroy(catsnatch_rfid_t *rfid)
 
 static int catsnatch_rfid_read(catsnatch_rfid_t *rfid)
 {
-	if ((rfid->bytes_read = read(rfid->fd, rfid->buf, sizeof(rfid->buf))) < 0)
+	int is_error = 0;
+	int errorcode;
+	const char *error_msg = NULL;
+
+	if ((rfid->bytes_read = read(rfid->fd, rfid->buf, 
+								sizeof(rfid->buf))) < 0)
 	{
 		if ((errno != EWOULDBLOCK) && (errno != EAGAIN))
 		{
 			fprintf(stderr, "Read error %d: %s\n", errno, strerror(errno));
 			return -1;
 		}
+
+		// TODO: Add CAT_NEED_MORE state here and handle that.
+		return -1;
 	}
 
-	return 0;
+	if (rfid->bytes_read == 0)
+	{
+		// TODO: EOF, do something special here?
+		return 0;
+	}
+
+	// Check for error.
+	if (rfid->buf[0] == '?')
+	{
+		is_error = 1;
+		errorcode = atoi(&rfid->buf[1]);
+		error_msg = catsnatch_rfid_error_str(errorcode);
+
+		fprintf(stderr, "%s RFID reader: Error %d on read: %s\n", 
+				rfid->name, errorcode, error_msg);
+	}
+
+	if (rfid->state == CAT_CONNECTED)
+	{
+		// We have issued a RAT (Read Animal Tag) command to the
+		// RFID reader, and are awaiting an OK.
+		if (!strncmp(rfid->buf, "OK", 2))
+		{
+			printf("Listening for Animal Tags on %s RFID reader", rfid->name);
+			rfid->state = CAT_AWAITING_TAG;
+			return 0;
+		}
+		else
+		{
+			fprintf(stderr, "Failed to issue RAT (Read Animal Tag) command.\n");
+			rfid->state = CAT_ERROR;
+			return -1;
+		}
+	}
+	else if (rfid->state == CAT_AWAITING_TAG)
+	{
+		char *end = strstr(rfid->buf, "\r\n");
+		int found = (end != NULL);
+
+		*end = '\0';
+		
+		if (!found)
+		{
+			fprintf(stderr, "Got incomplete tag string: %s\n", rfid->buf);
+		}
+		else
+		{
+			rfid->cb(rfid, rfid->buf);
+			return 0;
+		}
+	}
+
+	return -1;
 }
 
 int catsnatch_rfid_ctx_service(catsnatch_rfid_context_t *ctx)
 {
+	int i;
 	int res = 0;
 	struct timeval tv = {0, 0};
 
-	if (!ctx->rfid_in && !ctx->rfid_out)
+	if (!ctx->rfids[RFID_IN] && !ctx->rfids[RFID_OUT])
 	{
 		return -1;
 	}
 
-	if (ctx->rfid_in && (ctx->rfid_in->fd > 0))
+	for (i = 0; i < 2; i++)
 	{
-		FD_SET(ctx->rfid_in->fd, &ctx->readfs);
-	}
-
-	if (ctx->rfid_out && (ctx->rfid_out->fd > 0))
-	{
-		FD_SET(ctx->rfid_out->fd, &ctx->readfs);
+		if (ctx->rfids[i] && (ctx->rfids[i]->fd > 0))
+		{
+			FD_SET(ctx->rfids[i]->fd, &ctx->readfs);
+		}
 	}
 	
 	if (!(res = select(ctx->maxfd, &ctx->readfs, NULL, NULL, &tv)))
 	{
-		return 0;
+		return 0; // No input available.
 	}
 
-	if (ctx->rfid_in && FD_ISSET(ctx->rfid_in->fd, &ctx->readfs))
+	for (i = 0; i < 2; i++)
 	{
-		catsnatch_rfid_read(ctx->rfid_in);
-	}
+		if (ctx->rfids[i] && FD_ISSET(ctx->rfids[i]->fd, &ctx->readfs))
+		{
+			if (catsnatch_rfid_read(ctx->rfids[i]) < 0)
+			{
 
-	if (ctx->rfid_out && FD_ISSET(ctx->rfid_out->fd, &ctx->readfs))
-	{
-		catsnatch_rfid_read(ctx->rfid_out);
+			}
+		}
 	}
 
 	return 0;
@@ -86,31 +163,31 @@ int catsnatch_rfid_ctx_service(catsnatch_rfid_context_t *ctx)
 
 static void _set_maxfd(catsnatch_rfid_context_t *ctx)
 {
-	if (ctx->rfid_in && ctx->rfid_out)
+	if (ctx->rfids[RFID_IN] && ctx->rfids[RFID_OUT])
 	{
-		ctx->maxfd = MAX(ctx->rfid_out->fd, ctx->rfid_in->fd) + 1;
+		ctx->maxfd = MAX(ctx->rfids[RFID_IN]->fd, ctx->rfids[RFID_OUT]->fd) + 1;
 	}
-	else if (ctx->rfid_in)
+	else if (ctx->rfids[RFID_IN])
 	{
-		ctx->maxfd = ctx->rfid_in->fd + 1;
+		ctx->maxfd = ctx->rfids[RFID_IN]->fd + 1;
 	}
-	else if (ctx->rfid_out)
+	else if (ctx->rfids[RFID_OUT])
 	{
-		ctx->maxfd = ctx->rfid_out->fd + 1;
+		ctx->maxfd = ctx->rfids[RFID_OUT]->fd + 1;
 	}
 }
 
 void catsnatch_rfix_ctx_set_inner(catsnatch_rfid_context_t *ctx, catsnatch_rfid_t *rfid)
 {
 	assert(ctx);
-	ctx->rfid_in = rfid;
+	ctx->rfids[RFID_IN] = rfid;
 	_set_maxfd(ctx);
 }
 
 void catsnatch_rfid_ctx_set_outter(catsnatch_rfid_context_t * ctx, catsnatch_rfid_t *rfid)
 {
 	assert(ctx);
-	ctx->rfid_out = rfid;
+	ctx->rfids[RFID_OUT] = rfid;
 	_set_maxfd(ctx);
 }
 
--- catsnatch_rfid.h
+++ catsnatch_rfid.h
@@ -1,29 +1,53 @@
 
 #ifndef __CATSNATCH_RFID_H__
 #define __CATSNATCH_RFID_H__
+///
+/// Serial port communication with the Priority 1 Design
+/// http://www.priority1design.com.au/
+/// FDX-B/HDX RFID Reader Writer with external antenna and USB port
+///
+/// Datasheet: http://www.priority1design.com.au/rfidrw-e-usb.pdf
+///
 
 #include <termios.h>
 #include <signal.h>
 #include <unistd.h> 
 
-typedef void (*catsnatch_rfid_read_f)(int inner, const char *id);
-//typedef void (*catsnatch_rfid_signal_f)(int status);
+typedef struct catsnatch_rfid_s catsnatch_rfid_t;
+
+typedef void (*catsnatch_rfid_read_f)(catsnatch_rfid_t *rfid, const char *cat_tag);
+
+typedef enum catsnatch_rfid_state_e
+{
+	CAT_ERROR = -1,
+	CAT_DISCONNECTED = 0,
+	CAT_CONNECTED = 1,
+	CAT_AWAITING_TAG = 2,
+	CAT_NEED_MORE = 3
+} catsnatch_rfid_state_t;
+
+const char *catsnatch_rfid_error_str(int errorcode);
 
 typedef struct catsnatch_rfid_s
 {
+	char name[256];
 	const char *serial_path;
 	int fd;
 	ssize_t bytes_read;
+	ssize_t offset;
 	char buf[1024];
 	catsnatch_rfid_read_f cb;
+	catsnatch_rfid_state_t state;
 } catsnatch_rfid_t;
+
+#define RFID_IN 0
+#define RFID_OUT 1
 
 typedef struct catsnatch_rfid_context_s
 {
 	fd_set readfs;
 	int maxfd;
-	catsnatch_rfid_t *rfid_in;
-	catsnatch_rfid_t *rfid_out;
+	catsnatch_rfid_t *rfids[2];
 } catsnatch_rfid_context_t;
 
 #endif // __CATSNATCH_RFID_H__
