b70913845256d1e41ab27a1db8ed713bca496206
Joakim Söderberg <joakim.soderberg@gmail.com>
2014-03-15 14:56:34 +0100

    Woops had forgotten to add the new grabber.

---
+++ catcierge_grabber2.c
@@ -0,0 +1,300 @@
+
+#include "catcierge_config.h"
+#include "catcierge_args.h"
+#include "catcierge_log.h"
+#include "catcierge_util.h"
+#include <string.h>
+#include <assert.h>
+
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/highgui/highgui_c.h>
+
+#ifdef RPI
+#include "RaspiCamCV.h"
+#include "catcierge_gpio.h"
+#endif
+
+#ifdef WITH_RFID
+#include "catcierge_rfid.h"
+
+typedef struct rfid_match_s
+{
+	int triggered;			// Have this rfid matcher been triggered?
+	char data[128];			// The data in the match.
+	int incomplete;			// Is the data incomplete?
+	const char *time_str;	// Time of match.
+	int is_allowed;			// Is the RFID in the allowed list?
+} rfid_match_t;
+#endif // WITH_RFID
+
+// TODO: Fix printing fps.
+#if 0
+#define CATLOGFPS(fmt, ...) if (show_fps) { log_print(stdout, "%d fps  " fmt, 1, ##__VA_ARGS__); else CATLOG(fmt, ##__VA_ARGS__); }
+#define CATERRFPS(fmt, ...) if (show_fps) { log_print(stderr, "%d fps  " fmt, 1, ##__VA_ARGS__); else CATLOG(fmt, ##__VA_ARGS__); }
+#endif
+#define CATLOGFPS(fmt, ...) CATLOG(fmt, ##__VA_ARGS__)
+#define CATERRFPS(fmt, ...) CATLOG(fmt, ##__VA_ARGS__)
+
+
+typedef enum match_direction_e
+{
+	MATCH_DIR_UNKNOWN = -1,
+	MATCH_DIR_IN = 0,
+	MATCH_DIR_OUT = 1
+} match_direction_t;
+
+typedef struct catcierge_grb_s
+{
+	catcierge_args_t args;
+
+	int running;
+
+	#ifdef RPI
+	RaspiCamCvCapture *capture;
+	#else
+	CvCapture *capture;
+	#endif
+
+	#ifdef WITH_RFID
+	char *rfid_inner_path;
+	char *rfid_outer_path;
+	catcierge_rfid_t rfid_in;
+	catcierge_rfid_t rfid_out;
+	catcierge_rfid_context_t rfid_ctx;
+
+	match_direction_t rfid_direction;	// Direction that is determined based on which RFID reader gets triggered first.
+	rfid_match_t rfid_in_match;			// Match struct for the inner RFID reader.
+	rfid_match_t rfid_out_match;		// Match struct for the outer RFID reader.
+	char **rfid_allowed;				// The list of allowed RFID chips.
+	size_t rfid_allowed_count;
+	int lock_on_invalid_rfid;			// Should we lock when no or an invalid RFID tag is found?
+	double rfid_lock_time;				// The time after a camera match has been made until we check the RFID readers. (In seconds).
+	int checked_rfid_lock;				// Did we check if we should do an RFID lock during this match timeout?
+
+	#endif // WITH_RFID
+} catcierge_grb_t;
+
+static const char *match_get_direction_str(match_direction_t dir)
+{
+	switch (dir)
+	{
+		case MATCH_DIR_IN: return "in";
+		case MATCH_DIR_OUT: return "out";
+		case MATCH_DIR_UNKNOWN: 
+		default: return "unknown";
+	}
+}
+
+#ifdef WITH_RFID
+static void rfid_reset(rfid_match_t *m)
+{
+	memset(m, 0, sizeof(rfid_match_t));
+}
+
+static int match_allowed_rfid(catcierge_grb_t *grb, const char *rfid_tag)
+{
+	int i;
+	catcierge_args_t *args;
+	assert(grb);
+	args = &grb->args;
+
+	for (i = 0; i < args->rfid_allowed_count; i++)
+	{
+		if (!strcmp(rfid_tag, args->rfid_allowed[i]))
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+static void rfid_set_direction(catcierge_grb_t *grb, rfid_match_t *current, rfid_match_t *other, 
+						match_direction_t dir, const char *dir_str, catcierge_rfid_t *rfid, 
+						int incomplete, const char *data)
+{
+	catcierge_args_t *args;
+	assert(grb);
+	args = &grb->args;
+
+	CATLOGFPS("%s RFID: %s%s\n", rfid->name, data, incomplete ? " (incomplete)": "");
+
+	// Update the match if we get a complete tag.
+	if (current->incomplete &&  (strlen(data) > strlen(current->data)))
+	{
+		strcpy(current->data, data);
+		current->incomplete = incomplete;
+	}
+
+	// If we have already triggered this reader
+	// then don't set the direction again.
+	// (Since this could screw things up).
+	if (current->triggered)
+		return;
+
+	// The other reader triggered first so we know the direction.
+	// TODO: It could be wise to time this out after a while...
+	if (other->triggered)
+	{
+		grb->rfid_direction = dir;
+		CATLOGFPS("%s RFID: Direction %s\n", rfid->name, dir_str);
+	}
+
+	current->triggered = 1;
+	current->incomplete = incomplete;
+	strcpy(current->data, data);
+	current->is_allowed = match_allowed_rfid(grb, current->data);
+
+	//log_print_csv(log_file, "rfid, %s, %s\n", 
+	//		current->data, (current->is_allowed > 0)? "allowed" : "rejected");
+
+	catcierge_execute(args->rfid_detect_cmd, 
+		"%s %s %d %d %s %d %s",
+		rfid->name, 				// %0 = RFID reader name.
+		rfid->serial_path,			// %1 = RFID path.
+		current->is_allowed, 		// %2 = Is allowed.
+		current->incomplete, 		// %3 = Is data incomplete.
+		current->data,				// %4 = Tag data.
+		other->triggered,			// %5 = Other reader triggered.
+		match_get_direction_str(grb->rfid_direction)); // %6 = Direction.
+}
+
+static void rfid_inner_read_cb(catcierge_rfid_t *rfid, int incomplete, const char *data, void *user)
+{
+	catcierge_grb_t *grb = user;
+
+	// The inner RFID reader has detected a tag, we now pass that
+	// match on to the code that decides which direction the cat
+	// is going.
+	rfid_set_direction(grb, &grb->rfid_in_match, &grb->rfid_out_match,
+			MATCH_DIR_IN, "IN", rfid, incomplete, data);
+}
+
+static void rfid_outer_read_cb(catcierge_rfid_t *rfid, int incomplete, const char *data, void *user)
+{
+	catcierge_grb_t *grb = user;
+	rfid_set_direction(grb, &grb->rfid_out_match, &grb->rfid_in_match,
+			MATCH_DIR_OUT, "OUT", rfid, incomplete, data);
+}
+
+static void catcierge_init_rfid_readers(catcierge_grb_t *grb)
+{
+	catcierge_args_t *args;
+	assert(grb);
+
+	args = &grb->args;
+
+	catcierge_rfid_ctx_init(&grb->rfid_ctx);
+
+	if (args->rfid_inner_path)
+	{
+		catcierge_rfid_init("Inner", &grb->rfid_in, args->rfid_inner_path, rfid_inner_read_cb, grb);
+		catcierge_rfid_ctx_set_inner(&grb->rfid_ctx, &grb->rfid_in);
+		catcierge_rfid_open(&grb->rfid_in);
+	}
+
+	if (args->rfid_outer_path)
+	{
+		catcierge_rfid_init("Outer", &grb->rfid_out, args->rfid_outer_path, rfid_outer_read_cb, grb);
+		catcierge_rfid_ctx_set_outer(&grb->rfid_ctx, &grb->rfid_out);
+		catcierge_rfid_open(&grb->rfid_out);
+	}
+	
+	CATLOG("Initialized RFID readers\n");
+}
+#endif // WITH_RFID
+
+int catcierge_grabber_init(catcierge_grb_t *grb)
+{
+	assert(grb);
+
+	memset(grb, 0, sizeof(catcierge_grb_t));
+	
+	if (catcierge_args_init(&grb->args))
+	{
+		return -1;
+	}
+
+	return 0;
+}
+
+void catcierge_grabber_destroy(catcierge_grb_t *grb)
+{
+	if (grb->args.show)
+	{
+		cvDestroyWindow("catcierge");
+	}
+
+	catcierge_args_destroy(&grb->args);
+}
+
+void catcierge_setup_camera(catcierge_grb_t *grb)
+{
+	assert(grb);
+
+	if (grb->args.show)
+	{
+		cvNamedWindow("catcierge", 1);
+	}
+
+	#ifdef RPI
+	grb->capture = raspiCamCvCreateCameraCapture(0);
+	// TODO: Implement the cvSetCaptureProperty stuff below fo raspicamcv.
+	#else
+	grb->capture = cvCreateCameraCapture(0);
+	cvSetCaptureProperty(grb->capture, CV_CAP_PROP_FRAME_WIDTH, 320);
+	cvSetCaptureProperty(grb->capture, CV_CAP_PROP_FRAME_HEIGHT, 240);
+	#endif
+}
+
+void catcierge_destroy_camera(catcierge_grb_t *grb)
+{
+	#ifdef RPI
+	raspiCamCvReleaseCapture(&grb->capture);
+	#else
+	cvReleaseCapture(&grb->capture);
+	#endif
+}
+
+int main(int argc, char **argv)
+{
+	catcierge_grb_t grb;
+	fprintf(stderr, "\nCatcierge Grabber2 v" CATCIERGE_VERSION_STR " (C) Joakim Soderberg 2013-2014\n\n");
+
+	if (catcierge_grabber_init(&grb))
+	{
+		fprintf(stderr, "Failed to init\n");
+		return -1;
+	}
+
+	// Get program settings.
+	{
+		if (catcierge_parse_config(&grb.args, argc, argv))
+		{
+			catcierge_show_usage(&grb.args, argv[0]);
+			return -1;
+		}
+
+		if (catcierge_parse_cmdargs(&grb.args, argc, argv))
+		{
+			catcierge_show_usage(&grb.args, argv[0]);
+			return -1;
+		}
+
+		catcierge_print_settings(&grb.args);
+	}
+
+	catcierge_setup_camera(&grb);
+	CATLOG("Starting detection!\n");
+
+	do
+	{
+		// This does all the program logic.
+		//process_frames();
+	} while (grb.running);
+
+	catcierge_destroy_camera(&grb);
+	catcierge_args_destroy(&grb.args);
+
+	return 0;
+}
