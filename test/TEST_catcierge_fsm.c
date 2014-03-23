9ef466c52899d7fe0eba16fcdadd64d22e39657b
Joakim Söderberg <joakim.soderberg@gmail.com>
2014-03-23 15:04:35 +0100

    Add a test for max consecutive lockouts.

--- test/TEST_catcierge_fsm.c
+++ test/TEST_catcierge_fsm.c
@@ -25,6 +25,15 @@ static IplImage *open_test_image(int series, int i)
 	return img;
 }
 
+static void set_default_test_snouts(catcierge_args_t *args)
+{
+	assert(args);
+	args->snout_paths[0] = CATCIERGE_SNOUT1_PATH;
+	args->snout_count++;
+	args->snout_paths[1] = CATCIERGE_SNOUT2_PATH;
+	args->snout_count++;
+}
+
 static void free_test_image(catcierge_grb_t *grb)
 {
 	cvReleaseImage(&grb->img);
@@ -46,6 +55,88 @@ static int load_test_image_and_run(catcierge_grb_t *grb, int series, int i)
 	return 0;
 }
 
+static char *run_consecutive_lockout_abort_tests()
+{
+	int i;
+	catcierge_grb_t grb;
+	catcierge_args_t *args;
+	args = &grb.args;
+
+	catcierge_grabber_init(&grb);
+
+	args->saveimg = 0;
+	set_default_test_snouts(args);
+
+	if (catcierge_init(&grb.matcher, args->snout_paths, args->snout_count))
+	{
+		return "Failed to init catcierge lib!\n";
+	}
+
+	catcierge_set_match_flipped(&grb.matcher, 1);
+	catcierge_set_match_threshold(&grb.matcher, 0.8);
+
+	catcierge_set_state(&grb, catcierge_state_waiting);
+
+	args->max_consecutive_lockout_count = 3;
+	args->lockout_time = 0;
+
+	grb.running = 1;
+
+	// Now trigger "max consecutive lockouts" but add a success in the middle
+	// to make sure the consecutive count is reset.
+	{
+		load_test_image_and_run(&grb, 1, 2); // Obstruct.
+		load_test_image_and_run(&grb, 1, 2); // Pass 4 images (invalid).
+		load_test_image_and_run(&grb, 1, 3);
+		load_test_image_and_run(&grb, 1, 4);
+		load_test_image_and_run(&grb, 1, 4);
+		catcierge_run_state(&grb);
+		mu_assert("Expected 1 consecutive lockout count", (grb.consecutive_lockout_count == 1));
+
+		catcierge_test_STATUS("Consecutive lockout %d", 1);
+
+		load_test_image_and_run(&grb, 1, 2); // Obstruct.
+		load_test_image_and_run(&grb, 1, 2); // Pass 4 images (invalid).
+		load_test_image_and_run(&grb, 1, 3);
+		load_test_image_and_run(&grb, 1, 4);
+		load_test_image_and_run(&grb, 1, 4);
+		catcierge_run_state(&grb);
+		mu_assert("Expected 2 consecutive lockout count", (grb.consecutive_lockout_count == 2));
+
+		catcierge_test_STATUS("Consecutive lockout %d", 2);
+
+		load_test_image_and_run(&grb, 1, 1); // Obstruct.
+		load_test_image_and_run(&grb, 1, 1); // Pass 4 images (valid).
+		load_test_image_and_run(&grb, 1, 2);
+		load_test_image_and_run(&grb, 1, 3);
+		load_test_image_and_run(&grb, 1, 4);
+		load_test_image_and_run(&grb, 1, 5); // Clear frame.
+		mu_assert("Expected reset consecutive lockout count", (grb.consecutive_lockout_count == 0));
+
+		catcierge_test_STATUS("Consecutive lockout reset");
+
+		load_test_image_and_run(&grb, 1, 2); // Obstruct.
+		load_test_image_and_run(&grb, 1, 2); // Pass 4 images (invalid).
+		load_test_image_and_run(&grb, 1, 3);
+		load_test_image_and_run(&grb, 1, 4);
+		load_test_image_and_run(&grb, 1, 4);
+		catcierge_run_state(&grb);
+		mu_assert("Expected 1 lockout count", (grb.consecutive_lockout_count == 1));
+
+		catcierge_test_STATUS("Consecutive lockout %d", 1);
+	}
+
+	mu_assert("Expected program to be in running state after consecutive lockout count is aborted", (grb.running == 1));
+
+	// Now trigger "max consecutive lockouts" but add a success in the middle
+	// to make sure the consecutive count is reset.
+
+	catcierge_destroy(&grb.matcher);
+	catcierge_grabber_destroy(&grb);
+
+	return NULL;
+}
+
 static char *run_consecutive_lockout_tests()
 {
 	int i;
@@ -56,10 +147,7 @@ static char *run_consecutive_lockout_tests()
 	catcierge_grabber_init(&grb);
 
 	args->saveimg = 0;
-	args->snout_paths[0] = CATCIERGE_SNOUT1_PATH;
-	args->snout_count++;
-	args->snout_paths[1] = CATCIERGE_SNOUT2_PATH;
-	args->snout_count++;
+	set_default_test_snouts(args);
 
 	if (catcierge_init(&grb.matcher, args->snout_paths, args->snout_count))
 	{
@@ -112,10 +200,7 @@ static char *run_failure_tests(int obstruct)
 	catcierge_grabber_init(&grb);
 
 	args->saveimg = 0;
-	args->snout_paths[0] = CATCIERGE_SNOUT1_PATH;
-	args->snout_count++;
-	args->snout_paths[1] = CATCIERGE_SNOUT2_PATH;
-	args->snout_count++;
+	set_default_test_snouts(args);
 
 	if (catcierge_init(&grb.matcher, args->snout_paths, args->snout_count))
 	{
@@ -242,17 +327,22 @@ int TEST_catcierge_fsm(int argc, char **argv)
 	// Same as above, but add an extra frame obstructing at the end.
 	catcierge_test_STATUS("Run success tests. With obstruct");
 	if ((e = run_success_tests(1))) { catcierge_test_FAILURE(e); ret = -1; }
-	else catcierge_test_SUCCESS("");
+	else catcierge_test_SUCCESS("Success match with obstruct");
 
 	// Pass a set of images known to fail.
 	catcierge_test_STATUS("Run failure tests.");
 	if ((e = run_failure_tests(0))) { catcierge_test_FAILURE(e); ret = -1; }
-	else catcierge_test_SUCCESS("");
+	else catcierge_test_SUCCESS("Failure tests");
 
 	// Trigger max consecutive lockout.
 	catcierge_test_STATUS("Run consecutive lockout tests.");
 	if ((e = run_consecutive_lockout_tests())) { catcierge_test_FAILURE(e); ret = -1; }
-	else catcierge_test_SUCCESS("");
+	else catcierge_test_SUCCESS("Consecuive lockout tests");
+
+	// Trigger max consecutive lockout.
+	catcierge_test_STATUS("Run consecutive lockout test. Reset counter");
+	if ((e = run_consecutive_lockout_abort_tests())) { catcierge_test_FAILURE(e); ret = -1; }
+	else catcierge_test_SUCCESS("Consecuive lockout test (with reset counter)");
 
 	return ret;
 }
