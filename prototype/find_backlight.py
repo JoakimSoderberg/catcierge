fbafc07a343fa96ae03b7bed13e73e7516871efe
Joakim Söderberg <joakim.soderberg@gmail.com>
2016-04-21 19:45:02 +0200

    Fix backlight detection prototype

--- prototype/find_backlight.py
+++ prototype/find_backlight.py
@@ -9,14 +9,16 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("images", metavar = "IMAGES", nargs = 1,
-                    help = "The Catcierge match images to test.")
+parser.add_argument("images", metavar="IMAGES", nargs=1,
+                    help="The Catcierge match images to test.")
+parser.add_argument("--threshold", "-t", type=int, default=90,
+					help="The threshold value")
 
 args = parser.parse_args()
 
 img = cv2.imread(args.images[0])
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, threshimg = cv2.threshold(img_gray, 100, 255, 0)
+ret, threshimg = cv2.threshold(img_gray, args.threshold, 255, 0)
 
 contours, hierarchy = cv2.findContours(threshimg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 max_area = 0
@@ -30,7 +32,7 @@ r = cv2.boundingRect(max_contour)
 print r
 x, y, w, h = r
 cv2.drawContours( img, contours, -1, (0, 255, 0), 3 )
-cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
+cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
 cv2.imwrite("sq.png", img)
 cv2.imwrite("thr.png", threshimg)
