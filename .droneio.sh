bb8294686f6b7fe5e1e7cd46853f4e5bde2c6efd
Joakim Soderberg <joakim.soderberg@netrounds.com>
2016-07-25 18:50:20 +0200

    Update drone io build file

--- .droneio.sh
+++ .droneio.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 git submodule update --recursive --init
 sudo apt-get update -qq
-sudo apt-get install -y -qq valgrind libopencv-dev libzmq-dev
+sudo apt-get install -y -qq valgrind libopencv-dev libzmq-dev libczmq3
 mkdir build
 cd build
 cmake ..
