02bf4ad80618194ce8708f6022a2e086d5b7d3b9
Joakim Soderberg <joakim.soderberg@netrounds.com>
2016-07-25 19:02:52 +0200

    Stupid ubuntu

--- .droneio.sh
+++ .droneio.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git submodule update --recursive --init
 #sudo apt-get update -qq
-sudo apt-get install -y -qq valgrind libopencv-dev libzmq-dev libczmq-dev
+sudo apt-get install -y -qq valgrind libopencv-dev libzmq-dev
 mkdir build
 cd build
 set -e
-cmake -DWITH_ZMQ=ON ..
+cmake -DWITH_ZMQ=OFF ..
 make
 ctest --output-on-failure
