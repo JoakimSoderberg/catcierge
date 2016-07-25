f48609f980228c4b5ed479829dc4c516d04cb339
Joakim Soderberg <joakim.soderberg@netrounds.com>
2016-07-25 19:04:33 +0200

    stupid ubuntu missing zmq

--- .droneio.sh
+++ .droneio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git submodule update --recursive --init
-#sudo apt-get update -qq
+sudo apt-get update -qq
 sudo apt-get install -y -qq valgrind libopencv-dev libzmq-dev
 mkdir build
 cd build
