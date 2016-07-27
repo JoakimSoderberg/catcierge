14a4b08a59615f6dfa20ac1e7f070e9e72d82df7
Joakim Soderberg <joakim.soderberg@netrounds.com>
2016-07-27 17:48:52 +0200

    pass extra args in a nicer way

--- extra/catcierge_rand_fsm_tester.py
+++ extra/catcierge_rand_fsm_tester.py
@@ -35,8 +35,9 @@ def main():
 
 	parser.add_argument("--exe_path", help="Path to catcierge_fsm_tester executable")
 	parser.add_argument("--image_dir", help="Directory of images")
-	parser.add_argument("--extra_args", help="Extra arguments to pass", default="")
+	#parser.add_argument("--extra_args", help="Extra arguments to pass", default="")
 	parser.add_argument("--fail", help="Only get a random fail image", action="store_true")
+	parser.add_argument("extra_args", nargs=argparse.REMAINDER)
 
 	args = parser.parse_args()
 
@@ -55,9 +56,9 @@ def main():
 		file_glob = "%s*.png" % rand_file[:-5]  # .../all/match__2014-09-23_17_15_10__
 		images = glob.glob(file_glob)
 
-	extra_args = [os.path.expanduser(os.path.expandvars(s)) for s in args.extra_args.split(" ")]
+	#extra_args = [os.path.expanduser(os.path.expandvars(s)) for s in args.extra_args.split(" ")]
 
-	the_args = [args.exe_path] + ["--images"] + images + extra_args
+	the_args = [args.exe_path] + ["--images"] + images + args.extra_args
 	print(the_args)
 
 	call(the_args)
