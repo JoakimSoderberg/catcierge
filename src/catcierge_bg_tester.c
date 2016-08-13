5e76f94dd24efc98667f8c5999c64c58e01d7446
Joakim Soderberg <joakim.soderberg@gmail.com>
2016-08-13 18:26:39 +0200

    Add interactive support for tweaking threshold

--- src/catcierge_bg_tester.c
+++ src/catcierge_bg_tester.c
@@ -28,6 +28,7 @@
 #include "catcierge_output.h"
 #include "test/catcierge_test_common.h"
 #include "catcierge_strftime.h"
+#include "catcierge_log.h"
 #include <opencv2/imgproc/imgproc_c.h>
 #include <opencv2/highgui/highgui_c.h>
 
@@ -39,9 +40,8 @@
 
 typedef struct bg_tester_ctx_s
 {
-	//char *img_paths[MAX_IMG_PATHS];
 	char *img_path;
-	//size_t img_count;
+	int interactive;
 } bg_tester_ctx_t;
 
 bg_tester_ctx_t ctx;
@@ -71,10 +71,11 @@ static int add_bg_tester_args(catcierge_args_t *args)
 			"when tweaking the settings relating to finding the backlight "
 			"in the background using --auto_roi and other command line options.\n");
 
-	/*ret |= cargo_add_option(cargo, CARGO_OPT_REQUIRED,
-			"<bg> --images",
-			"Input images that are passed to catcierge.",
-			".[s]#", &ctx.img_paths, &ctx.img_count, MAX_IMG_PATHS);*/
+	ret |= cargo_add_option(cargo, 0,
+		"<bg> --interactive",
+		"GUI required, allows changing the auto roi threshold interactively.",
+		"b", &ctx.interactive);
+
 	ret |= cargo_add_option(cargo, CARGO_OPT_REQUIRED,
 		"<bg> image",
 		"Input image containig the background image used to tweak settings with.",
@@ -119,8 +120,76 @@ int main(int argc, char **argv)
 
 	printf("Image: %s\n", ctx.img_path);
 
+	if (!(grb.img = load_image(&grb, ctx.img_path)))
+	{
+		fprintf(stderr, "Failed to load background image %s\n", ctx.img_path);
+		ret = -1; goto fail;
+	}
+
+	if (ctx.interactive)
+	{
+		cvNamedWindow("Auto ROI", CV_WINDOW_AUTOSIZE);
+	}
+
+
+
+	grb.running = 1;
+
+	while (grb.running)
+	{
+		CvRect roi;
+		int success = 0;
+
+		if (catcierge_matcher_init(&grb.matcher, catcierge_get_matcher_args(args)))
+		{
+			fprintf(stderr, "\n\nFailed to %s init matcher\n\n", grb.matcher->name);
+			goto fail;
+		}
+
+		if (catcierge_get_back_light_area(grb.matcher, grb.img, &roi))
+		{
+			fprintf(stderr, "Failed to find back light area\n");
+			if (ctx.interactive)
+			{
+				cvShowImage("Auto ROI", grb.img);
+			}
+
+			success = 0;
+		}
+
+		// Get threshold from trackbar.
+		if (ctx.interactive)
+		{
+			cvWaitKey(10);
+
+			// We recreate the trackbar each time, since if the user closes the window it won't
+			// be recreated otherwise when the window is recreated.
+			cvCreateTrackbar("thr", "Auto ROI", &args->auto_roi_thr, 255, NULL);
+			args->auto_roi_thr = cvGetTrackbarPos("thr", "Auto ROI");
+			printf("Trackbar: %d\n", args->auto_roi_thr);
+		}
+
+		if (success)
+		{
+			cvSetImageROI(grb.img, roi);
+
+			if (catcierge_is_frame_obstructed(grb.matcher, grb.img))
+			{
+				CATERR("Frame obstructed\n");
+			}			
+		}
+
+		catcierge_matcher_destroy(&grb.matcher);
+
+		if (!ctx.interactive)
+		{
+			printf("Not in --interactive mode (GUI), exiting...\n");
+			break;
+		}
+	}
 
 fail:
+	cvDestroyAllWindows();
 	catcierge_grabber_destroy(&grb);
 	catcierge_args_destroy(&grb.args);
 
