0edd86d2626f86c99192b8d5e3185da9d3ec42e8
Joakim Söderberg <joakim.soderberg@gmail.com>
2017-08-29 23:21:57 +0200

    Fix warning for old zmq

--- src/catcierge_fsm.h
+++ src/catcierge_fsm.h
@@ -116,7 +116,7 @@ typedef struct catcierge_grb_s
 
 	#ifdef WITH_ZMQ
 	#if (ZMQ_VERSION < ZMQ_MAKE_VERSION (4, 0, 0))
-	void *zmq_ctx;
+	zctx_t *zmq_ctx;
 	#endif
 	void *zmq_pub;	// ZMQ publisher.
 	#endif // WITH_ZMQ
